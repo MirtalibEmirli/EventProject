0023931debf3a1feab82a621c230853db47f4d13
Iman011 <imaneyvazov2@gmail.com>
2025-04-04 14:13:35 +0400

    login added

--- Core/EventProject.Application/Features/Commands/UserCommands/Register/RegisterCommand.cs
+++ Core/EventProject.Application/Features/Commands/UserCommands/Register/RegisterCommand.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;  
 
 namespace EventProject.Application.Features.Commands.UserCommands.Register
 {

b92c46469d73e9b6a19c25d96a5a477a0adcbe00
MirtalibEmirli <amirli_kl34@itstep.edu.az>
2025-05-08 20:05:10 +0400

    bug fix olundu

--- Core/EventProject.Application/Features/Commands/EventStandingZoneCommand/DeleteStandingZone/DeleteStandingZoneHandler.cs
+++ Core/EventProject.Application/Features/Commands/EventStandingZoneCommand/DeleteStandingZone/DeleteStandingZoneHandler.cs
@@ -1,29 +1,29 @@
 ﻿using EventProject.Application.Repositories.StandingZones;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace EventProject.Application.Features.Commands.EventStandingZoneCommand.DeleteStandingZone
+namespace EventProject.Application.Features.Commands.EventStandingZoneCommand.DeleteStandingZone;
+
+public class DeleteStandingZoneHandler : IRequestHandler<DeleteStandingZoneRequest, bool>
 {
-    public class DeleteStandingZoneHandler : IRequestHandler<DeleteStandingZoneRequest, bool>
+    private readonly IStandingZoneWriteRepository standingZoneWriteRepository;
+
+    private readonly IStandingZoneReadRepository standingZoneReadRepository;
+
+    public DeleteStandingZoneHandler(IStandingZoneWriteRepository standingZoneWriteRepository, IStandingZoneReadRepository standingZoneReadRepository)
     {
-        private readonly IStandingZoneWriteRepository standingZoneWriteRepository;
+        this.standingZoneWriteRepository = standingZoneWriteRepository;
+        this.standingZoneReadRepository = standingZoneReadRepository;
+    }
 
-        private readonly IStandingZoneReadRepository standingZoneReadRepository;
+    public async Task<bool> Handle(DeleteStandingZoneRequest request, CancellationToken cancellationToken)
+    {
+        var zone = await standingZoneReadRepository.GetByIdAsync(request.Id);
+        if(zone == null) return false;
 
-        public async Task<bool> Handle(DeleteStandingZoneRequest request, CancellationToken cancellationToken)
-        {
-            var zone = await standingZoneReadRepository.GetByIdAsync(request.Id);
-            if(zone == null) return false;
-
-            await standingZoneWriteRepository.SoftDeleteAsync(request.Id);
-            await standingZoneWriteRepository.SaveChangesAsync();
-            return true;
+        await standingZoneWriteRepository.SoftDeleteAsync(request.Id);
+        await standingZoneWriteRepository.SaveChangesAsync();
+        return true;
 
 
-        }
     }
 }
